a55b836813a69e39d74cf4aaaa1d1b51bb2de559
Yogakshem787 <yashdangi33@gmail.com>
2026-02-25 11:12:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ from flask import Flask, request, jsonify, g
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 import jwt as pyjwt
+import requests
 
 # ═══════ RATE LIMITER ═══════
 _rate_limits = {}
